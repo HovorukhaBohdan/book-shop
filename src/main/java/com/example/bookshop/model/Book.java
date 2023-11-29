534b846cc32165f0b56a1074f19866d9aba25d41
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-11-29 09:22:22 +0200

    Created book entity and BookRepository interface

---
+++ src/main/java/com/example/bookshop/model/Book.java
@@ -0,0 +1,29 @@
+package com.example.bookshop.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String author;
+    @Column(nullable = false, unique = true)
+    private String isbn;
+    @Column(nullable = false)
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+}
---
+++ src/main/java/com/example/bookshop/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,18 @@
+package com.example.bookshop.repository.impl;
+
+import com.example.bookshop.model.Book;
+import com.example.bookshop.repository.BookRepository;
+
+import java.util.List;
+
+public class BookRepositoryImpl implements BookRepository {
+    @Override
+    public Book save(Book book) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return null;
+    }
+}
