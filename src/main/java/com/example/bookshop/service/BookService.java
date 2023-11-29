ab044ec3f8ee730dcc0a10d72a0ce5d2aeceead2
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-11-29 13:37:01 +0200

    Added BookService

---
+++ src/main/java/com/example/bookshop/service/BookService.java
@@ -0,0 +1,11 @@
+package com.example.bookshop.service;
+
+import com.example.bookshop.model.Book;
+
+import java.util.List;
+
+public interface BookService {
+    Book save(Book book);
+
+    List<Book> findAll();
+}
---
+++ src/main/java/com/example/bookshop/service/impl/BookServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.bookshop.service.impl;
+
+import com.example.bookshop.model.Book;
+import com.example.bookshop.repository.BookRepository;
+import com.example.bookshop.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
