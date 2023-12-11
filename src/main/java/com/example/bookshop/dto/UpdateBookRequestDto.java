c618df5f2d7b749f27a2f56565b330cf5eff8762
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-12-11 12:40:38 +0200

    Added needed annotations to DTO's

--- src/main/java/com/example/bookshop/dto/UpdateBookRequestDto.java
+++ src/main/java/com/example/bookshop/dto/UpdateBookRequestDto.java
@@ -1,13 +1,20 @@
 package com.example.bookshop.dto;
 
 import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class UpdateBookRequestDto {
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotNull
     private String isbn;
+    @Positive
     private BigDecimal price;
     private String description;
     private String coverImage;
