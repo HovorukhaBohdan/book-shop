6f3ff82469ff2146017e4eb2e76128ea90490789
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-12-12 20:22:19 +0200

    Fixed messages

--- src/main/java/com/example/bookshop/dto/UpdateBookRequestDto.java
+++ src/main/java/com/example/bookshop/dto/UpdateBookRequestDto.java
@@ -7,14 +7,14 @@ import lombok.Data;
 
 @Data
 public class UpdateBookRequestDto {
-    @NotNull(message = "Field 'title' can't be null")
+    @NotNull(message = "can't be null")
     private String title;
-    @NotNull(message = "Field 'author' can't be null")
+    @NotNull(message = "can't be null")
     private String author;
-    @NotNull(message = "Field 'isbn' can't be null")
+    @NotNull(message = "can't be null")
     private String isbn;
-    @Positive(message = "Field 'price' must be more than 0")
-    @NotNull(message = "Field 'price' can't be null")
+    @Positive(message = "must be more than 0")
+    @NotNull(message = "can't be null")
     private BigDecimal price;
     private String description;
     private String coverImage;
