cb18f0f1a7516fb679f00519e4229270680d4d17
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-12-17 22:00:03 +0200

    Implemented register method in AuthenticationController

--- src/main/java/com/example/bookshop/controller/AuthenticationController.java
+++ src/main/java/com/example/bookshop/controller/AuthenticationController.java
@@ -2,15 +2,20 @@ package com.example.bookshop.controller;
 
 import com.example.bookshop.dto.user.UserRegistrationRequestDto;
 import com.example.bookshop.dto.user.UserResponseDto;
+import com.example.bookshop.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/auth")
 public class AuthenticationController {
+    private final UserService userService;
+
     @PostMapping("/registeration")
     public UserResponseDto register(UserRegistrationRequestDto requestDto) {
-        return null;
+        return userService.register(requestDto);
     }
 }
