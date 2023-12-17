ccfdec13a116b552baf8ed1b31689bf5a6f92252
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-12-17 21:39:34 +0200

    Created user entity, changed @NonNull to @Column(nullable = false) in book model

---
+++ src/main/java/com/example/bookshop/model/User.java
@@ -0,0 +1,22 @@
+package com.example.bookshop.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(name = "shipping_address")
+    private String shippingAddress;
+}
