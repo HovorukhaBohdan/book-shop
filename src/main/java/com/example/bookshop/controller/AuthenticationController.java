efe1273ff2e161a39fe4559b0c42dc87a49a3ad2
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-12-17 22:41:09 +0200

    Added file for creating users table

--- src/main/java/com/example/bookshop/controller/AuthenticationController.java
+++ src/main/java/com/example/bookshop/controller/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.example.bookshop.controller;
 
 import com.example.bookshop.dto.user.UserRegistrationRequestDto;
 import com.example.bookshop.dto.user.UserResponseDto;
+import com.example.bookshop.exception.RegistrationException;
 import com.example.bookshop.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,8 @@ public class AuthenticationController {
     private final UserService userService;
 
     @PostMapping("/registeration")
-    public UserResponseDto register(UserRegistrationRequestDto requestDto) {
+    public UserResponseDto register(UserRegistrationRequestDto requestDto)
+            throws RegistrationException {
         return userService.register(requestDto);
     }
 }
