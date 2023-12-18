4ef2b079af36b5e61ebcdc344215db80b4ac5504
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-12-18 20:49:10 +0200

    Created role model

---
+++ src/main/java/com/example/bookshop/model/Role.java
@@ -0,0 +1,20 @@
+package com.example.bookshop.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private RoleName name;
+
+    private enum RoleName {
+        ADMIN,
+        USER
+    }
+}
