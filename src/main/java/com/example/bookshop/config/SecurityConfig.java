1be8043ac47ea8a59c1d30b758b1025a26967c0d
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-12-19 09:05:41 +0200

    Fixed bugs

--- src/main/java/com/example/bookshop/config/SecurityConfig.java
+++ src/main/java/com/example/bookshop/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.example.bookshop.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,10 +13,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableMethodSecurity
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
     private final UserDetailsService userDetailsService;
+
+    @Value("${publicEndpoints}")
+    private String[] publicEndpoints;
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
@@ -28,7 +34,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth
-                                .requestMatchers("/api/auth/**, /error,/swagger-ui/**")
+                                .requestMatchers(publicEndpoints)
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
--- src/main/java/com/example/bookshop/model/User.java
+++ src/main/java/com/example/bookshop/model/User.java
@@ -1,14 +1,7 @@
 package com.example.bookshop.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.util.Collection;
 import java.util.Set;
 import lombok.Data;
@@ -38,7 +31,7 @@ public class User implements UserDetails {
     private String shippingAddress;
     @Column(name = "is_deleted", nullable = false)
     private Boolean isDeleted = false;
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "users_roles",
                joinColumns = @JoinColumn(name = "user_id"),
                inverseJoinColumns = @JoinColumn(name = "role_id"))
