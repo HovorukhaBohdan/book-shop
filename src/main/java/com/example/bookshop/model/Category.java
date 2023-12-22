affd58c404aed85bfe5de3dec0c1c9453bccdcc4
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-12-22 20:44:49 +0200

    Created category entity

---
+++ src/main/java/com/example/bookshop/model/Category.java
@@ -0,0 +1,16 @@
+package com.example.bookshop.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    private String description;
+}
