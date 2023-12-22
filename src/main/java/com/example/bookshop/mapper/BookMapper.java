849996b69176f6575273de026c1dd6042d54e4b7
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-12-22 21:05:52 +0200

    Added setCategoryIds method to Book mapper

--- src/main/java/com/example/bookshop/mapper/BookMapper.java
+++ src/main/java/com/example/bookshop/mapper/BookMapper.java
@@ -6,8 +6,15 @@ import com.example.bookshop.dto.book.BookDtoWithoutCategoryIds;
 import com.example.bookshop.dto.book.CreateBookRequestDto;
 import com.example.bookshop.dto.book.UpdateBookRequestDto;
 import com.example.bookshop.model.Book;
+import com.example.bookshop.model.Category;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(config = MapperConfig.class, uses = {CategoryMapper.class})
 public interface BookMapper {
@@ -21,4 +28,12 @@ public interface BookMapper {
     Book toBookEntity(UpdateBookRequestDto bookRequestDto);
 
     BookDtoWithoutCategoryIds toDtoWithoutCategories(Book book);
+
+    @AfterMapping
+    default void setCategoryIds(@MappingTarget BookDto bookDto, Book book) {
+        Set<Long> categoryIds = book.getCategories().stream()
+                .map(Category::getId)
+                .collect(Collectors.toSet());
+        bookDto.setCategoryIds(categoryIds);
+    }
 }
