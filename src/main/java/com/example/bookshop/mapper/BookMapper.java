6df18451a0d3aa42bdc64a0cb575ce3dc5cf8757
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-12-22 21:03:11 +0200

    Modifies book dto's and book mapper

--- src/main/java/com/example/bookshop/mapper/BookMapper.java
+++ src/main/java/com/example/bookshop/mapper/BookMapper.java
@@ -2,16 +2,23 @@ package com.example.bookshop.mapper;
 
 import com.example.bookshop.config.MapperConfig;
 import com.example.bookshop.dto.book.BookDto;
+import com.example.bookshop.dto.book.BookDtoWithoutCategoryIds;
 import com.example.bookshop.dto.book.CreateBookRequestDto;
 import com.example.bookshop.dto.book.UpdateBookRequestDto;
 import com.example.bookshop.model.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(config = MapperConfig.class)
+@Mapper(config = MapperConfig.class, uses = {CategoryMapper.class})
 public interface BookMapper {
+    @Mapping(target = "categoryIds")
     BookDto toBookDto(Book book);
 
+    @Mapping(source = "categoryIds", target = "categories")
     Book toBookEntity(CreateBookRequestDto bookRequestDto);
 
+    @Mapping(source = "categoryIds", target = "categories")
     Book toBookEntity(UpdateBookRequestDto bookRequestDto);
+
+    BookDtoWithoutCategoryIds toDtoWithoutCategories(Book book);
 }
