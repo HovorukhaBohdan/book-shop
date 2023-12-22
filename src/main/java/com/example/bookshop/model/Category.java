a8e271d5f4bdd21b26d4c75be47472d2be37428d
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-12-22 21:07:13 +0200

    Added isDeleted field to category entity

--- src/main/java/com/example/bookshop/model/Category.java
+++ src/main/java/com/example/bookshop/model/Category.java
@@ -2,9 +2,13 @@ package com.example.bookshop.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE users SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted=false")
 @Table(name = "categories")
 public class Category {
     @Id
@@ -13,4 +17,6 @@ public class Category {
     @Column(nullable = false)
     private String name;
     private String description;
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted = false;
 }
