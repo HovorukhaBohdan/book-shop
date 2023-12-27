177c4d11006b42d11a81b2a41a103d4ccf095237
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-12-27 22:05:55 +0200

    Fixed checkstyle

--- src/main/java/com/example/bookshop/dto/item/CartItemRequestDto.java
+++ src/main/java/com/example/bookshop/dto/item/CartItemRequestDto.java
@@ -1,7 +1,6 @@
 package com.example.bookshop.dto.item;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
