535592c7f63c0e3238ef533c44317a0591dbd6c3
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2024-01-07 22:45:55 +0200

    Added tests for shopping cart service

--- src/main/java/com/example/bookshop/dto/item/CartItemRequestDto.java
+++ src/main/java/com/example/bookshop/dto/item/CartItemRequestDto.java
@@ -3,8 +3,10 @@ package com.example.bookshop.dto.item;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class CartItemRequestDto {
     @Min(value = 1, message = "bookId value must be more than 0")
     @NotNull(message = "bookId mustn't be null")
--- src/main/java/com/example/bookshop/dto/item/UpdateRequestCartItemDto.java
+++ src/main/java/com/example/bookshop/dto/item/UpdateRequestCartItemDto.java
@@ -2,8 +2,10 @@ package com.example.bookshop.dto.item;
 
 import jakarta.validation.constraints.Min;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class UpdateRequestCartItemDto {
     @Min(value = 1, message = "quantity value must be more than 0")
     private int quantity;
